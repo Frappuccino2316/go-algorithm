6529bc42a850d36ee63539932926aad94de42468
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-17 22:11:11 +0900

    各packageをimport

--- main.go
+++ main.go
@@ -2,45 +2,21 @@ package main
 
 import (
 	"fmt"
+	"go-algorithm/cerror"
+	"go-algorithm/search"
 )
 
 func main() {
 	a := []int{3, 2, 6, 9, 1}
 
-	i, err := lsm(a, 1)
+	i, err := search.Lsm(a, 1)
 	if err != nil {
-		e, ok := err.(*nothingError)
+		e, ok := err.(*cerror.NothingError)
 		if ok {
-			fmt.Println(e.message)
+			fmt.Println(e.Message)
 		}
 	} else {
 		fmt.Printf("index: %v\n", i)
 		fmt.Printf("value: %v\n", a[i])
 	}
-}
-
-type nothingError struct {
-	message string
-}
-
-func (e *nothingError) Error() string {
-	return e.message
-}
-
-func raiseError() error {
-	return &nothingError{message: "存在しませんでした"}
-}
-
-func lsm(a []int, b int) (int, error) {
-	fmt.Printf("target: %v\n", a)
-	fmt.Printf("search: %v\n", b)
-
-	for i, n := range a {
-		if n == b {
-			fmt.Println("みつけた!")
-			return i, nil
-		}
-		continue
-	}
-	return -1, raiseError()
 }

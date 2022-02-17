02d53b035d0fa1c5d07269c62ae72d9999ec5718
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-17 21:56:31 +0900

    エラーを切り出す

---
+++ err/nothing.go
@@ -0,0 +1,13 @@
+package err
+
+type NothingError struct {
+	message string
+}
+
+func (e *NothingError) Error() string {
+	return e.message
+}
+
+func RaiseError() error {
+	return &NothingError{message: "存在しませんでした"}
+}

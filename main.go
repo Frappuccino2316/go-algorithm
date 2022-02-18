7c95da0be6c951055db8c7bfdd9cf508fb6f7ab0
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-18 22:52:16 +0900

    ハッシュ

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 
+	// "github.com/frappuccino2316/go-algorithm/cerror"
 	"github.com/frappuccino2316/go-algorithm/cerror"
 	"github.com/frappuccino2316/go-algorithm/search"
 )
 
-// 二分探索法 Binary search method
+// ハッシュ探索法 hashing method
 func main() {
-	a := []int{1, 2, 4, 6, 11, 15, 17, 18, 21, 50, 55, 67, 73, 99, 100}
+	a := []int{1, 8, 11, 15, 17, 18, 21, 37}
+	h := search.Hashing(a)
 
-	i, err := search.Bsm(a, 101)
+	i, err := search.Hm(h, 15)
+
 	if err != nil {
 		e, ok := err.(*cerror.NothingError)
 		if ok {
@@ -19,9 +22,25 @@ func main() {
 		}
 	} else {
 		fmt.Printf("index: %v\n", i)
-		fmt.Printf("value: %v\n", a[i])
+		fmt.Printf("value: %v\n", h[i])
 	}
 }
+
+// 二分探索法 Binary search method
+// func main() {
+// 	a := []int{1, 2, 4, 6, 11, 15, 17, 18, 21, 50, 55, 67, 73, 99, 100}
+
+// 	i, err := search.Bsm(a, 101)
+// 	if err != nil {
+// 		e, ok := err.(*cerror.NothingError)
+// 		if ok {
+// 			fmt.Println(e.Message)
+// 		}
+// 	} else {
+// 		fmt.Printf("index: %v\n", i)
+// 		fmt.Printf("value: %v\n", a[i])
+// 	}
+// }
 
 // 線形探索法 Linear search method
 // func main() {
--- search/hash.go
+++ search/hash.go
@@ -1,27 +1,47 @@
 package search
 
-// import (
-// 	"fmt"
-// 	"time"
+import (
+	"fmt"
+	"time"
 
-// 	"github.com/frappuccino2316/go-algorithm/cerror"
-// )
+	"github.com/frappuccino2316/go-algorithm/cerror"
+)
 
 func Hashing(a []int) []int {
 	length := len(a)
-	var h []int
+	h := make([]int, length*2)
+	fmt.Println(len(h))
+
 	for _, n := range a {
 		k := n % (length + 1)
 		if h[k] == 0 {
 			h[k] = n
 		} else {
-			for i := k + 1; i == (k + length); i++ {
+			for i := k + 1; i < length*2; i++ {
 				if h[i] == 0 {
 					h[i] = n
+					break
 				}
-				continue
 			}
 		}
 	}
 	return h
+}
+
+func Hm(h []int, b int) (int, error) {
+	time.Sleep(time.Second)
+	fmt.Printf("target: %v\n", h)
+	fmt.Printf("search: %v\n", b)
+
+	length := len(h)
+	index := b % (length/2 + 1)
+
+	for i := index; i < length; i++ {
+		fmt.Printf("i: %v\n", i)
+		fmt.Printf("h[i]: %v\n", h[i])
+		if h[i] == b {
+			return i, nil
+		}
+	}
+	return -1, cerror.RaiseError()
 }
